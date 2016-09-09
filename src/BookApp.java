7b4c0fb3f0190beffd19a274c6f19207baa7fa91
Sindhukumar <ssundar6@gmu.edu>
2016-09-09 11:19:16 -0400

    First Commit

---
+++ src/BookApp.java
@@ -0,0 +1,10 @@
+
+public class BookApp {
+	public static void main(String args[]) {
+		Book b1= new Book();
+		b1.setAuthor("Aname");
+		b1.setTitle("Atitle");
+		b1.setDescription("Adescription");
+		b1.getDisplayText();
+	}
+}
